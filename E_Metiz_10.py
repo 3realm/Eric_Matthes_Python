cda6a245caebe98018dcf8dcfe148181d10bbb5a
Nikita <nikitaboriakov@yandex.ru>
2024-04-01 19:14:04 +0300

    10-3 - 10-5

--- E_Metiz_10.py
+++ E_Metiz_10.py
@@ -1,6 +1,8 @@
 # 10-1
 # with open('text_files/имя_файла.txt') as file_object:
 file_name_1 = '10_text_files/learning_python.txt'
+file_name_2 = '10_text_files/guest.txt'
+file_name_3 = '10_text_files/answer.txt'
 
 with open(file_name_1) as file_object_1:
     contents = file_object_1.read()
@@ -24,3 +26,15 @@ with open(file_name_1) as file_object_2:
     for line in lines:
         if 'Python' in line:
             print(line.replace('Python', change_string))
+# 10-3
+# string_name = input('Input your name: ') + "\n"
+# with open(file_name_2, 'a') as file_object_3:
+#     file_object_3.write(string_name)
+# 10-4
+# 10-5
+stop_flag = '1'
+while stop_flag != '0':
+    string_reason = input('Input your reason: ') + "\n"
+    with open(file_name_3, 'a') as file_object_3:
+        file_object_3.write(string_reason)
+    stop_flag = input('Input 0 for EXIT')
